6ba7e614206daaa0b17591a318c06198b2755815
maoyongjun <H7108464@foxconn.com>
2018-09-28 17:04:29 +0800

    工站前后台交互代码分析， 增加天津L5的出货工站

---
+++ MESStation/Stations/StationActions/DataLoaders/DNLoader.cs
@@ -0,0 +1,31 @@
+﻿using MESDataObject;
+using MESPubLab.MESStation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MESStation.Stations.StationActions.DataLoaders
+{
+    class DNLoader
+    {
+        public static void DNByPOLoader(MESPubLab.MESStation.MESStationBase Station, MESPubLab.MESStation.MESStationInput Input, List<MESDataObject.Module.R_Station_Action_Para> Paras)
+        {
+            if (Paras.Count == 0)
+            {
+                throw new Exception(MESReturnMessage.GetMESReturnMessage("MES00000050"));
+            }
+            MESStationSession s = Station.StationSession.Find(t => t.MESDataType == Paras[0].SESSION_TYPE && t.SessionKey == Paras[0].SESSION_KEY);
+            if (s == null)
+            {
+                s = new MESStationSession() { MESDataType = Paras[0].SESSION_TYPE, InputValue = Input.Value.ToString(), SessionKey = Paras[0].SESSION_KEY, ResetInput = Input };
+                Station.StationSession.Add(s);
+            }
+
+            string IntData = Input.Value.ToString();
+
+        }
+
+    }
+}
